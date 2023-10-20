f632e0a21e3b038bc6f28c2a5b2e52717a3a66b1
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2023-10-20 15:44:18 +0600

    user crud

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ActivityController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,6 +28,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('users',UserController::class)->names('users');
+    Route::resource('activities',ActivityController::class)->names('activities');
+
 });
 
 require __DIR__.'/auth.php';
